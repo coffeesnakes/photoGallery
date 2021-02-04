0d25c5738b5fb5dde1e5b1f56f32a16e6b7428ce
Jacob Berg <coffeesnakes@gmail.com>
2021-02-03 23:41:47 -0800

    schema working in psql database

--- database/schema.sql
+++ database/schema.sql
@@ -1,42 +1,39 @@
 DROP SCHEMA IF EXISTS photoGallery CASCADE;
 CREATE SCHEMA IF NOT EXISTS photoGallery AUTHORIZATION sdc_user;
 
-CREATE TABLE IF NOT EXISTS gallery (
+CREATE TABLE IF NOT EXISTS photoGallery.gallery (
   id SERIAL PRIMARY KEY,
   isSuperhost Boolean,
-  reviews BINARY,
-  rating INTEGER,
-)
+  reviews BOOLEAN,
+  rating INTEGER
+);
 
-CREATE TABLE IF NOT EXISTS homes (
+CREATE TABLE IF NOT EXISTS photoGallery.homes (
   home_id  SERIAL PRIMARY KEY,
   city VARCHAR(135),
   usState VARCHAR(100),
   country VARCHAR(100),
   rating INT,
-  superhost BINARY,
-  photos INTEGER,
+  superhost BOOLEAN,
+  photos INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS photos (
+CREATE TABLE IF NOT EXISTS photoGallery.photos (
   photo_id  SERIAL PRIMARY KEY,
-  user_id INTEGER,
+  usr_id INTEGER,
   photo_url VARCHAR(225),
   upload_date TIMESTAMP,
-  caption VARCHAR(1000),
-
+  caption VARCHAR(1000)
 );
 
-CREATE TABLE IF NOT EXISTS users (
-  user_id  SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS photoGallery.usr (
+  usr_id  SERIAL PRIMARY KEY,
   username VARCHAR(35),
   email VARCHAR(50),
   hashed_password VARCHAR(200),
-  ip VARCHAR(30),
-  PRIMARY KEY (user_id),
+  ip VARCHAR(30)
 );
 
 
-ALTER TABLE Home ADD FOREIGN KEY (home_id) REFERENCES gallery (id);
-ALTER TABLE Home ADD FOREIGN KEY (photos) REFERENCES Photos (photo_id);
-ALTER TABLE Photos ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
+ALTER TABLE photoGallery.homes ADD FOREIGN KEY (home_id) REFERENCES photoGallery.gallery (id);
+ALTER TABLE photoGallery.homes ADD FOREIGN KEY (photos) REFERENCES photoGallery.Photos (photo_id);
